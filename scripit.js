82dd9dc8e362940dd371737945355a12e039cd5d
thaxdúcaapaaaaaa <francisco.almeida527@aluno.ce.gov.br>
2025-09-02 16:00:41 -0300

    Update scripit.js

--- scripit.js
+++ scripit.js
@@ -2,14 +2,15 @@
 let nome =  prompt("digite o seu nome").toLowerCase()
 let idade = parseInt(prompt("digite o sua idade")) 
 //verificação de usuário
-if (idade >18) {
+if (idade >= 18) {
     alert (`Olá, ${nome}! Você tem acesso completo. Boas-vindas à plataforma!.`)
 }else if (nome === "admin"){
     alert (`Bem-vindo, ${admin}! Acesso total liberado`)
 }else if (nome=== "moderador") {
     alert (`Olá, ${moderador} ! Acesso liberado para a área de moderação.`)
-}else if (idade <17) {
+}else if (idade <=17) {
     alert ("Acesso negado. Você pode participar do nosso programa Jovem Aprendiz")
-}else if (idade <13) {
+}else if (idade < 13) {
     alert("Acesso negado. O conteúdo é exclusivo para maiores de 18 anos.")
 }
+
